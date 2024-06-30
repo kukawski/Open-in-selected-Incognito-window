9c34138cbdc4f46033ebd7e3302c2ba5a3c5edb7
Rafał Kukawski <rafal@kukawski.pl>
2024-06-30 16:25:02 +0200

    Code formatting

--- src/background.js
+++ src/background.js
@@ -1,125 +1,129 @@
-const getTitle = (window) => window.incognito ? window.title.replace(/ ?-[^-]+$/, '') : window.title;
+const getTitle = (window) =>
+  window.incognito ? window.title.replace(/ ?-[^-]+$/, "") : window.title;
 
 const getUserSettings = async () => {
-    const defaultSettings = {
-        includeNormalWindows: false,
-        unnestSingleWindow: false,
-        activateTab: false,
-        excludeCurrentWindow: false
-    };
+  const defaultSettings = {
+    includeNormalWindows: false,
+    unnestSingleWindow: false,
+    activateTab: false,
+    excludeCurrentWindow: false,
+  };
 
-    return browser.storage.local.get(defaultSettings);;
-}
+  return browser.storage.local.get(defaultSettings);
+};
 
 const populateContextMenu = async () => {
-    await browser.menus.removeAll();
+  await browser.menus.removeAll();
 
-    const userSettings = await getUserSettings();
+  const userSettings = await getUserSettings();
 
-    let windows = await browser.windows.getAll({
-        windowTypes: ['normal']
+  let windows = await browser.windows.getAll({
+    windowTypes: ["normal"],
+  });
+
+  console.log("windows", windows.length);
+
+  if (!userSettings.includeNormalWindows) {
+    windows = windows.filter((w) => w.incognito);
+  }
+
+  if (userSettings.excludeCurrentWindow) {
+    const currentWindow = await browser.windows.getCurrent();
+
+    windows = windows.filter((w) => w.id !== currentWindow.id);
+  }
+
+  if (userSettings.unnestSingleWindow && windows.length === 1) {
+    const window = windows[0];
+
+    browser.contextMenus.create({
+      id: `incognito-selection-window-${window.id}`,
+      title: browser.i18n.getMessage("openLinkIn", getTitle(window)),
+      contexts: ["link"],
+    });
+  } else {
+    const rootMenuItemId = browser.contextMenus.create({
+      id: `incognito-selection`,
+      title: browser.i18n.getMessage("openLinkInSelectedPrivateWindow"),
+      contexts: ["link"],
     });
 
-    console.log("windows", windows.length);
+    if (windows.length) {
+      const incognitoIcon = {
+        16: "incognito.svg",
+      };
 
-    if (!userSettings.includeNormalWindows) {
-        windows = windows.filter(w => w.incognito);
-    }
-
-    if (userSettings.excludeCurrentWindow) {
-        const currentWindow = await browser.windows.getCurrent();
-
-        windows = windows.filter(w => w.id !== currentWindow.id);
-    }
-
-    if (userSettings.unnestSingleWindow && windows.length === 1) {
-        const window = windows[0];
-
+      windows.forEach((window) => {
         browser.contextMenus.create({
-            id: `incognito-selection-window-${window.id}`,
-            title: browser.i18n.getMessage('openLinkIn', getTitle(window)),
-            contexts: ['link']
+          id: `incognito-selection-window-${window.id}`,
+          title: getTitle(window),
+          contexts: ["link"],
+          parentId: rootMenuItemId,
+          icons:
+            userSettings.includeNormalWindows && window.incognito
+              ? incognitoIcon
+              : null,
         });
-    } else {
-        const rootMenuItemId = browser.contextMenus.create({
-            id: `incognito-selection`,
-            title: browser.i18n.getMessage('openLinkInSelectedPrivateWindow'),
-            contexts: ['link']
-        });
+      });
 
-        if (windows.length) {
-            const incognitoIcon = {
-                '16': 'incognito.svg'
-            };
-
-            windows.forEach((window) => {
-                browser.contextMenus.create({
-                    id: `incognito-selection-window-${window.id}`,
-                    title: getTitle(window),
-                    contexts: ['link'],
-                    parentId: rootMenuItemId,
-                    icons: userSettings.includeNormalWindows && window.incognito ? incognitoIcon : null
-                });
-            });
-
-            browser.contextMenus.create({
-                id: 'separator',
-                type: 'separator',
-                contexts: ['link'],
-                parentId: rootMenuItemId
-            });
-        }
-
-        if (userSettings.includeNormalWindows) {
-            browser.contextMenus.create({
-                id: `new-normal-window`,
-                title: browser.i18n.getMessage('openLinkInNewWindow'),
-                contexts: ['link'],
-                parentId: rootMenuItemId
-            });
-        }
-
-        browser.contextMenus.create({
-            id: `new-incognito-window`,
-            title: browser.i18n.getMessage('openLinkInNewPrivateWindow'),
-            contexts: ['link'],
-            parentId: rootMenuItemId
-        });
+      browser.contextMenus.create({
+        id: "separator",
+        type: "separator",
+        contexts: ["link"],
+        parentId: rootMenuItemId,
+      });
     }
-}
+
+    if (userSettings.includeNormalWindows) {
+      browser.contextMenus.create({
+        id: `new-normal-window`,
+        title: browser.i18n.getMessage("openLinkInNewWindow"),
+        contexts: ["link"],
+        parentId: rootMenuItemId,
+      });
+    }
+
+    browser.contextMenus.create({
+      id: `new-incognito-window`,
+      title: browser.i18n.getMessage("openLinkInNewPrivateWindow"),
+      contexts: ["link"],
+      parentId: rootMenuItemId,
+    });
+  }
+};
 
 browser.menus.onClicked.addListener(async (info, tab) => {
-    if (info.menuItemId.startsWith('incognito-selection-window-')) {
-        const { activateTab } = await getUserSettings();
+  if (info.menuItemId.startsWith("incognito-selection-window-")) {
+    const { activateTab } = await getUserSettings();
 
-        browser.tabs.create({
-            url: info.linkUrl,
-            windowId: +info.menuItemId.substr('incognito-selection-window-'.length),
-            active: activateTab
-        });
-    } else if (info.menuItemId === 'new-incognito-window') {
-        browser.windows.create({
-            incognito: true,
-            url: info.linkUrl
-        });
-    } else if (info.menuItemId === 'new-normal-window') {
-        browser.windows.create({
-            incognito: false,
-            url: info.linkUrl
-        });
-    }
+    browser.tabs.create({
+      url: info.linkUrl,
+      windowId: +info.menuItemId.substr("incognito-selection-window-".length),
+      active: activateTab,
+    });
+  } else if (info.menuItemId === "new-incognito-window") {
+    browser.windows.create({
+      incognito: true,
+      url: info.linkUrl,
+    });
+  } else if (info.menuItemId === "new-normal-window") {
+    browser.windows.create({
+      incognito: false,
+      url: info.linkUrl,
+    });
+  }
 });
 
 if (browser.menus.onShown && browser.menus.refresh) {
-    browser.menus.onShown.addListener(async (info, tab) => {
-        await populateContextMenu();
-        browser.menus.refresh();
-    });
+  browser.menus.onShown.addListener(async (info, tab) => {
+    await populateContextMenu();
+    browser.menus.refresh();
+  });
 } else {
-    browser.windows.onCreated.addListener(populateContextMenu);
-    browser.windows.onRemoved.addListener(populateContextMenu);
-    browser.tabs.onActivated.addListener(populateContextMenu);
-    browser.tabs.onUpdated.addListener(populateContextMenu);
+  browser.windows.onCreated.addListener(populateContextMenu);
+  browser.windows.onRemoved.addListener(populateContextMenu);
+  browser.tabs.onActivated.addListener(populateContextMenu);
+  browser.tabs.onUpdated.addListener(populateContextMenu);
 
-    populateContextMenu();
+  populateContextMenu();
 }
--- src/settings.js
+++ src/settings.js
@@ -1,24 +1,29 @@
 (async function () {
-    document.querySelector('form').addEventListener('change', (event) => {
-        browser.storage.local.set({
-            includeNormalWindows: document.getElementById('includeNormalWindows').checked,
-            unnestSingleWindow: document.getElementById('unnestSingleWindow').checked,
-            activateTab: document.getElementById('activateTab').checked,
-            excludeCurrentWindow: document.getElementById('excludeCurrentWindow').checked
-        });
+  document.querySelector("form").addEventListener("change", (event) => {
+    browser.storage.local.set({
+      includeNormalWindows: document.getElementById("includeNormalWindows")
+        .checked,
+      unnestSingleWindow: document.getElementById("unnestSingleWindow").checked,
+      activateTab: document.getElementById("activateTab").checked,
+      excludeCurrentWindow: document.getElementById("excludeCurrentWindow")
+        .checked,
     });
+  });
 
-    const defaultSettings = {
-        includeNormalWindows: false,
-        unnestSingleWindow: false,
-        activateTab: false,
-        excludeCurrentWindow: false
-    };
+  const defaultSettings = {
+    includeNormalWindows: false,
+    unnestSingleWindow: false,
+    activateTab: false,
+    excludeCurrentWindow: false,
+  };
 
-    const userSettings = await browser.storage.local.get(defaultSettings);
+  const userSettings = await browser.storage.local.get(defaultSettings);
 
-    document.getElementById('includeNormalWindows').checked = userSettings.includeNormalWindows;
-    document.getElementById('unnestSingleWindow').checked = userSettings.unnestSingleWindow;
-    document.getElementById('activateTab').checked = userSettings.activateTab;
-    document.getElementById('excludeCurrentWindow').checked = userSettings.excludeCurrentWindow;
-}());
+  document.getElementById("includeNormalWindows").checked =
+    userSettings.includeNormalWindows;
+  document.getElementById("unnestSingleWindow").checked =
+    userSettings.unnestSingleWindow;
+  document.getElementById("activateTab").checked = userSettings.activateTab;
+  document.getElementById("excludeCurrentWindow").checked =
+    userSettings.excludeCurrentWindow;
+})();
